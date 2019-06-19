4d3733506ce99a7345fe8c0767ec172d04fb8041
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-19 10:35:52 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+require 'pry'
+class School
+    attr_reader :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        roster[grade] ||= [] # If left roster[grade] is false or nil create [] as value
+        roster[grade] << student
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        roster.each do |key, students|
+            students.sort!
+        end
+    end
+end
